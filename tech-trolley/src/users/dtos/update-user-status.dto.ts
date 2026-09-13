e5a75d58e7142eb065b885cedb982293ec43e253
skmumit11 <asad713637@gmail.com>
2026-09-14 05:23:50 +0600

    Delete previos backend

--- tech-trolley/src/users/dtos/update-user-status.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsBoolean, IsNotEmpty } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class UpdateUserStatusDto {
-  @ApiProperty({ example: false })
-  @IsBoolean()
-  @IsNotEmpty()
-  isActive: boolean;
-}
